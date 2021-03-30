287a7ee5c287dbbfe00b903d289fc19323bdf68e
lazio <diniovskiy@gmail.com>
2021-03-30 13:54:38 +0300

    review comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 import styles from './App.module.scss';
 import { Table } from './Table/Table';
 
+// can you add our eslint, stylelint, prettier?
+// https://github.com/Halo-Lab/prettier-config-halo-lab
+// https://github.com/Halo-Lab/stylelint-config-halo-lab
+// https://github.com/Halo-Lab/eslint-config-halo-lab - I can see this one in devDependencies, but I you also need to add config file, see the documentation
+
 function App() {
+
+  // I think headers and data should be in the same array, so it'll be clear that they're connected
+  // const tableColumns = [{
+  //   header: 'name',
+  //   data: ['adidas', 'nike', 'puma', 'reebok']
+  // },{
+  //   header: 'second',
+  //   data: [21, 22, 23, 24]
+  // },{
+  //   header: 'third',
+  //   data: [31, 32, 33]
+  // },
+  // {
+  //   header: 'fourth',
+  //   data: [, 42, 43, 44]
+  // }]
+
+
   const headers = ['name', 'second', 'third', 'fourth'];
   const data = [
     { name: 'adidas', second: 21, third: 31 },
@@ -11,8 +34,10 @@ function App() {
     { name: 'reebok', second: 24, fourth: 44 },
   ];
 
+  // typo Ceil -> Cell
   const emptyCeil = '';
 
+  // I think it'll be better to let users to override styles in CSS by classnames
   const styleObj = {
     addColumnButton: {
       backgroundColor: '#000',
@@ -43,6 +68,7 @@ function App() {
   }
   
   return (
+    // do we need styles.btn here? 
     <div className={styles.btn}>
       <Table 
         headers={headers} 
